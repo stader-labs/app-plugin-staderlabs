b89c5f6669249952e171f6181272ccdb107cd51a
Manoj Patra <manoj9april@gmail.com>
2022-12-15 13:23:34 +0530

    Fix compiler warnings (#8)
    * fix compiler warnings
    
    * endline

--- src/handle_finalize.c
+++ src/handle_finalize.c
@@ -2,7 +2,6 @@
 
 void handle_finalize(void *parameters) {
     ethPluginFinalize_t *msg = (ethPluginFinalize_t *) parameters;
-    context_t *context = (context_t *) msg->pluginContext;
 
     msg->uiType = ETH_UI_TYPE_GENERIC;
     msg->numScreens = 1;
