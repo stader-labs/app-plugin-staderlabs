f30b35b1a45c49ae5d7a0a288ee05a7f432f8b4b
Manoj Patra <manoj9april@gmail.com>
2022-12-15 13:20:02 +0530

    fix compiler warnings

--- src/handle_provide_token.c
+++ src/handle_provide_token.c
@@ -5,6 +5,5 @@
 // find any info regarding the requested tokens!
 void handle_provide_token(void *parameters) {
     ethPluginProvideInfo_t *msg = (ethPluginProvideInfo_t *) parameters;
-    context_t *context = (context_t *) msg->pluginContext;
     msg->result = ETH_PLUGIN_RESULT_OK;
 }
