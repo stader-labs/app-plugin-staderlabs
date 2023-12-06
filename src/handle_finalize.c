476e25cde9e0a9e1646dbef08a541b133efe1e66
Manoj Patra <github@manoj982.anonaddy.com>
2023-12-06 18:17:34 +0530

    set the ui on screens

--- src/handle_finalize.c
+++ src/handle_finalize.c
@@ -8,6 +8,7 @@ void handle_finalize(ethPluginFinalize_t *msg) {
         case ETHX_DEPOSIT_LEGACY:
         case ETHX_REQUEST_WITHDRAW:
         case ETHX_REQUEST_WITHDRAW_LEGACY:
+        case ETHX_BOOST_REWARDS_CLAIM:
             msg->numScreens = 2;
             break;
 
