498c01e0f0009a1fe301bb655fe55687f929e41b
Manoj Patra <github@manoj982.anonaddy.com>
2023-12-21 19:54:10 +0530

    lint fix

--- src/contract.c
+++ src/contract.c
@@ -127,7 +127,7 @@ static const uint32_t ETHX_REQUEST_WITHDRAW_LEGACY_SELECTOR = 0xccc143b8;
 // Network: Ethereum
 // Contract: MerkleDistributor
 // Method: function claim(uint256 index, address account, uint256 amount, bytes32[] calldata
-// merkleProof) 
+// merkleProof)
 // Selector: 0x2e7ba6ef
 static const uint32_t ETHX_BOOST_REWARDS_CLAIM_SELECTOR = 0x2e7ba6ef;
 
